087676a7e8db8021b1f761b613a656a8209f0b6b
S00300 <rikuto.maehira@smart-group.co.jp>
2026-05-12 11:41:23 +0900

    jsでレスポンス処理をする

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,14 +1,38 @@
-function post(){//post という名前の関数宣言
+const buildHTML = (XHR) => {
+  const item = XHR.response;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.createdAt}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
+function post (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", () => {
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST","/posts",true);//送り方.宛先.非同期にするか？-true
+    XHR.open("POST", "/posts", true);  //
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if(XHR.status != 200) {
+        alert(`Error${XHR.status}:${XHR.response.error}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
-window.addEventListener('load',post);//ブラウザ画面全体.イベント発生見守り.ロードしたときに.post関数を動かしてね
+window.addEventListener('load', post);
